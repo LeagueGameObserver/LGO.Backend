bfb55759da148751cb4f53ee405dffecc3e2f1a6
Johannes Schneider <johannes-schneider_1995@web.de>
2021-04-08 17:37:33 +0200

    add some test for full client game deserialization

--- LGO.LeagueOfLegends.ClientApiTest/Model/LolClientGameTest.cs
+++ LGO.LeagueOfLegends.ClientApiTest/Model/LolClientGameTest.cs
@@ -1,7 +1,9 @@
-﻿using System.IO;
+﻿using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using LGO.Backend.Core.Model.LeagueOfLegends.Enum;
 using LGO.LeagueOfLegends.ClientApi.Model.Game.Internal;
+using LGO.LeagueOfLegends.ClientApi.Model.GameEvent;
 using Newtonsoft.Json;
 using NUnit.Framework;
 
@@ -11,22 +13,65 @@ namespace LGO.LeagueOfLegends.ClientApiTest.Model
     public class LolClientGameTest
     {
         [Test]
+        public void TestDeserializeClassicGame()
+        {
+            var game = JsonConvert.DeserializeObject<MutableGame>(File.ReadAllText("Resource/FullClassicGame.json"));
+
+            Assert.NotNull(game);
+
+            Assert.NotNull(game.ActivePlayer);
+            Assert.AreEqual("Active Player", game.ActivePlayer.SummonerName);
+            Assert.AreEqual(3439.0d, game.ActivePlayer.CurrentGold, 1.0d);
+
+            Assert.NotNull(game.Stats);
+            Assert.AreEqual(LolGameModeType.Classic, game.Stats.GameMode);
+
+            Assert.NotNull(game.EventCollection);
+            Assert.AreEqual(70, game.EventCollection.Events.Count());
+
+            var events = new List<ILolClientGameEvent>(game.EventCollection.Events);
+
+            Assert.AreEqual(LolTurretTierType.BottomOuter, (events[27] as ILolClientTurretDestroyedEvent)?.Turret.Tier);
+            Assert.AreEqual(LolTeamType.Red, (events[27] as ILolClientTurretDestroyedEvent)?.Turret.Team);
+
+            Assert.AreEqual(LolGameResult.Win, (game.EventCollection.Events.Last() as ILolClientGameEndedEvent)?.ResultForActivePlayer);
+        }
+
+        [Test]
         public void TestDeserializeClassicBotGame()
         {
-            var game = JsonConvert.DeserializeObject<MutableGame>(
-                File.ReadAllText("Resource/FullClassicGameWithBots.json"));
+            var game = JsonConvert.DeserializeObject<MutableGame>(File.ReadAllText("Resource/FullClassicGameWithBots.json"));
 
             Assert.NotNull(game);
 
             Assert.NotNull(game.ActivePlayer);
             Assert.AreEqual("Human Summoner", game.ActivePlayer.SummonerName);
             Assert.AreEqual(5542.0d, game.ActivePlayer.CurrentGold, 1.0d);
-            
+
             Assert.NotNull(game.Stats);
             Assert.AreEqual(LolGameModeType.Classic, game.Stats.GameMode);
-            
+
             Assert.NotNull(game.EventCollection);
             Assert.AreEqual(78, game.EventCollection.Events.Count());
+        }
+
+        [Test]
+        public void TestDeserializeSpectatedClassicGame()
+        {
+            var game = JsonConvert.DeserializeObject<MutableGame>(File.ReadAllText("Resource/FullClassicSpectatedGame.json"));
+
+            Assert.NotNull(game);
+
+            Assert.NotNull(game.ActivePlayer);
+            Assert.AreEqual(string.Empty, game.ActivePlayer.SummonerName);
+            Assert.AreEqual(0.0d, game.ActivePlayer.CurrentGold);
+
+            Assert.NotNull(game.Stats);
+            Assert.AreEqual(LolGameModeType.Classic, game.Stats.GameMode);
+
+            Assert.NotNull(game.EventCollection);
+            Assert.AreEqual(9, game.EventCollection.Events.Count());
+            Assert.AreEqual(LolGameResult.Win, (game.EventCollection.Events.Last() as ILolClientGameEndedEvent)?.ResultForActivePlayer);
         }
     }
 }
